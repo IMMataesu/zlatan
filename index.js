f9162f6b632f1319388d7d431c025e54e0db0697
IMMataesu <kepluto28@gmail.com>
2025-01-15 10:42:46 +0900

    fix

---
+++ index.js
@@ -0,0 +1,11 @@
+setInterval(() =>{
+
+
+
+
+let imgs = document.querySelectorAll('img');
+imgs.forEach((a, i)=>{
+    a.src = 'https://i.namu.wiki/i/rbOAzxvUuSSNv1VuxDAZn6t1e5Q-uf_MqqeyuQKRNLKpC0sQaKaxyLzhqIlRyEdOUpce9eMOzDxuN7MwWVcBzQ.webp'
+})
+
+}, 500)
